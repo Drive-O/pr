19b1ade253e40394692f69ca2f9cad739cd30aca
Philipp Rost <PhilSantay@gmail.com>
2017-02-09 19:40:01 +0100

    Added Contact page and change js

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,38 +1,43 @@
 const hamburger = document.querySelector(".first-page__nav__hamburger");
 const menu = document.querySelector(".first-page__nav__menu");
-const about = document.querySelector(".second-page");
+const startTextContainer = document.querySelector(".first-page__hl__txt");
+const aboutTextContainer = document.querySelector(".second-page__container__about-me");
+const aboutText = aboutTextContainer.textContent;
+const startText = "Hi. I'm Philipp.";
+const aboutContainer = document.querySelector(".second-page");
 
 hamburger.addEventListener("click", function(){
   hamburger.classList.toggle("active");
   menu.classList.toggle("active");
 })
-window.addEventListener("scroll", function(){
-  console.log(about.offsetTop);
-  console.log("window:" + window.pageYOffset);
-  if(window.pageYOffset >= about.offsetTop){
-    console.log("Yes");
-  }
-})
 
-const app = {
-  content: document.querySelector(".first-page__hl__txt"),
-  text: "Hi. I'm Philipp.",
-  index: 0,
-  chars: 0,
-  speed: 100,
-  init: function(){
+function Typewriter(container, text, speed){
+    this.container = container;
+    this.text = text;
+    this.speed = speed;
     this.chars = this.text.length;
-    return this.write();
-  },
-  write: function(){
-    this.content.append(this.text[this.index]);
-    if(this.index < this.chars - 1){
-      this.index++;
-      return setTimeout(function(){
-        return app.write();
-      }, this.speed);
+    this.index = 0;
+
+    this.write = function(){
+      if(this.index < this.chars){
+        this.container.append(this.text[this.index]);
+        this.index++;
+        let that = this;
+        return setTimeout(function(){
+          return that.write();
+        }, this.speed)
+      }
     }
-  }
 }
 
-window.onload(app.init());
+const startTyper = new Typewriter(startTextContainer, startText, 100);
+startTyper.write();
+
+const aboutTyper = new Typewriter(aboutTextContainer, aboutText, 100);
+
+window.addEventListener("scroll", function(){
+  if(window.pageYOffset >= aboutContainer.offsetTop){
+    //start Typer
+    aboutTyper.write();
+  }
+})
